b8b82fa7de146a23c9088b0ca5a352caf80e5d44
tykim <lovehjty@gmail.com>
2021-07-19 23:48:22 +0900

    first commit

---
+++ 00.torch_dataset.py
@@ -0,0 +1,11 @@
+#API활용
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+
+#1. data
+
+torch.utils.data.DataLoder
